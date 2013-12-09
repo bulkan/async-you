68ed7953830b1fbedd4212e3305ca3f6b8514292
Bulkan Evcimen <bulkan@gmail.com>
2013-12-09 22:51:17 +1100

    wrote the solution for first problem

--- problems/waterfall/setup.js
+++ problems/waterfall/setup.js
@@ -1,30 +1,16 @@
 var http  = require('http');
 
-function server (i, delay, port) {
-  return http.createServer(function (req, res) {
-    writeWords(i, delay, res)
-  }).listen(port)
-}
-
 module.exports = function () {
-  var servers = [
-      server(0, 100, 9345)
-    , server(1, 0,   9346)
-    , server(2, 50,  9347)
-  ]
+  var server = http.createServer(function (req, res) {
+      res.end('boom!')
+  }).listen(9345)
 
   return {
       args  : [
-          'http://localhost:9345'
-        , 'http://localhost:9346'
-        , 'http://localhost:9347'
+        __dirname + '/url.txt'
       ]
     , stdin : null
     , long  : true
-    , close : function () {
-        servers.forEach(function (server) {
-          server.close()
-        })
-      }
+    , close : server.close.bind(server)
   }
 }
