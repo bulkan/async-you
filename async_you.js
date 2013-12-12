304bc443a3dbb0e5f8a0234ea7080f142a8a265c
Bulkan Evcimen <bulkan@gmail.com>
2013-12-13 01:32:24 +1100

    fixed title

--- async_you.js
+++ async_you.js
@@ -3,10 +3,9 @@
 const workshopper = require('workshopper')
   , path = require('path');
 
-
 workshopper({
   name: "async-you",
-  title: "async ",
+  title: "async you",
   subtitle: "Learn to use the async module in Node",
   appDir: __dirname,
   helpFile: path.join(__dirname, "help.txt")
