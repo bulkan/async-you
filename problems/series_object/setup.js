a0769577a81f08723512c87d80184afc564c7b43
Bulkan Evcimen <bulkan@gmail.com>
2013-12-12 14:26:18 +1100

    problem for learning async.series

---
+++ problems/series_object/setup.js
@@ -0,0 +1,17 @@
+var http  = require('http')
+  , url = require('url');
+
+module.exports = function () {
+  var server = http.createServer(function (req, res) {
+    if(url.parse(req.url).pathname === '/one')
+      return res.end('one is smaller than 2');
+    res.end('two greater than one');
+  }).listen(3131)
+
+  return {
+      args  : ['http://localhost:3131/one', 'http://localhost:3131/two']
+    , stdin : null
+    , long  : true
+    , close : server.close.bind(server)
+  }
+}
