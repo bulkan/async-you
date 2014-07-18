33745217eaa0a5699fc68717138a454d99b2383c
jkbits1 <jkbits1@users.noreply.github.com>
2014-07-18 04:07:26 +0100

    made changes so that verification is correct

--- problems/times/setup.js
+++ problems/times/setup.js
@@ -1,28 +1,46 @@
 var http = require('http');
 
 module.exports = function () {
-  var users = [];
 
-  var server = http.createServer(function (req, res) {
-    var body = "";
-    if (req.method.toLowerCase() === 'post') {
-      req.on('data', function(chuck){
-        body += chuck.toString();
-      });
+    var submissionUsers = [];
+    var solutionUsers   = [];
 
-      req.on('end', function(){
-        users.push(JSON.parse(body));
-        res.end();
-      });
-    } else {
-      res.end(JSON.stringify({'users': users}));
+    function handleRequests(req, res, users) {
+        var body = "";
+
+        if (req.method.toLowerCase() === 'post') {
+            req.on('data', function(chuck){
+                body += chuck.toString();
+            });
+
+            req.on('end', function(){
+
+                users.push(JSON.parse(body));
+                res.end();
+            });
+        } else {
+
+            res.end(JSON.stringify({'users': users}));
+        }
     }
-  }).listen(9345);
 
-  return {
-      args  : ['localhost', 9345]
+    var server = http.createServer(
+        function(req, res){
+            handleRequests(req, res, submissionUsers);
+        }
+    ).listen(9345);
+
+    var server = http.createServer(
+        function(req, res){
+            handleRequests(req, res, solutionUsers);
+        }
+    ).listen(9346);
+
+    return {
+       submissionArgs  : ['localhost', 9345]
+    ,  solutionArgs  : ['localhost', 9346]
     , stdin : null
     , long  : true
     , close : server.close.bind(server)
   }
-}
+};
