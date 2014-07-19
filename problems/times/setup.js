8ba87fa168c0b74c01b09833b7efaecd053bd13d
jkbits1 <jkbits1@users.noreply.github.com>
2014-07-19 02:23:48 +0100

    amended indentation and corrected close function

--- problems/times/setup.js
+++ problems/times/setup.js
@@ -1,46 +1,44 @@
 var http = require('http');
 
 module.exports = function () {
+  var submissionUsers = [];
+  var solutionUsers   = [];
 
-    var submissionUsers = [];
-    var solutionUsers   = [];
-
-    function handleRequests(req, res, users) {
-        var body = "";
-
-        if (req.method.toLowerCase() === 'post') {
-            req.on('data', function(chunk){
-                body += chunk.toString();
-            });
-
-            req.on('end', function(){
-
-                users.push(JSON.parse(body));
-                res.end();
-            });
-        } else {
-
-            res.end(JSON.stringify({'users': users}));
-        }
+  function handleRequests(req, res, users) {
+    var body = "";
+    if (req.method.toLowerCase() === 'post') {
+      req.on('data', function(chunk){
+        body += chunk.toString();
+      });
+      req.on('end', function(){
+        users.push(JSON.parse(body));
+        res.end();
+      });
+    } else {
+      res.end(JSON.stringify({'users': users}));
     }
+  }
 
-    var server = http.createServer(
-        function(req, res){
-            handleRequests(req, res, submissionUsers);
-        }
-    ).listen(9345);
+  var server1 = http.createServer(
+    function(req, res){
+      handleRequests(req, res, submissionUsers);
+    }
+  ).listen(9345);
 
-    var server = http.createServer(
-        function(req, res){
-            handleRequests(req, res, solutionUsers);
-        }
-    ).listen(9346);
+  var server2 = http.createServer(
+    function(req, res){
+      handleRequests(req, res, solutionUsers);
+      }
+  ).listen(9346);
 
-    return {
-       submissionArgs  : ['localhost', 9345]
-    ,  solutionArgs  : ['localhost', 9346]
+  return {
+      submissionArgs  : ['localhost', 9345]
+    , solutionArgs    : ['localhost', 9346]
     , stdin : null
     , long  : true
-    , close : server.close.bind(server)
+    , close : function() {
+        server1.close();
+        server2.close();
+      }
   }
 };
