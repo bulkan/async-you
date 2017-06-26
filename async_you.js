d2938f8c3aabd66e254d3742d4bc7c243dd4b7e0
Bulkan Evcimen <bulkan@gmail.com>
2017-06-26 23:55:31 +1000

    Switch to using workshopper-adventure
    * workshopper is not maintained anymore

--- async_you.js
+++ async_you.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
-const workshopper = require('workshopper')
-  , path = require('path');
+const workshopper = require('workshopper-adventure');
+const path = require('path');
 
-workshopper({
+const workshop = workshopper({
   name: "async-you",
   appDir: __dirname,
   languages: ['en', 'fr', 'ru'],
-  helpFile: path.join(__dirname, "./i18n/help/{lang}.txt")
+  helpFile: path.join(__dirname, "./i18n/help/{lang}.txt"),
+  header: require('workshopper-adventure/default/header'),
+  footer: require('workshopper-adventure/default/footer')
 });
+
+workshop.addAll(require('./exercises/menu.json'));
+workshop.execute(process.argv.slice(2));
